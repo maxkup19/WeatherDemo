6bb59820a7df1605ea4199eff7dab2d39da4300e
Maksym Kupchenko <maxkup19@gmail.com>
2022-07-05 19:17:08 +0200

    Initial Commit

---
+++ WeatherDemo/WeatherDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherDemoApp.swift
+//  WeatherDemo
+//
+//  Created by Max Kup on 05.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct WeatherDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
